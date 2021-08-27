be5ec48b4f5d8caf7d34628d5c9007c8bc217228
freddie41 <fcl41@me.com>
2021-08-27 17:11:22 -0300

    Cambios para mostrar user data post login normal y google sign in

--- js/init.js
+++ js/init.js
@@ -7,41 +7,61 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
-var showSpinner = function(){
+var showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
 }
 
-var hideSpinner = function(){
+var hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-var getJSONData = function(url){
-    var result = {};
-    showSpinner();
-    return fetch(url)
+var getJSONData = function (url) {
+  var result = {};
+  showSpinner();
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
     });
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+
+  // User data
+  var userLogged = localStorage.getItem("userLogged");
+  var userInfo = document.getElementById("userInfo");
+  var user = document.getElementById("user");
+
+  // Google user data
+  var googleUserData = onSignIn(googleUser);
+  var profile = googleUser.getBasicProfile();
+
+  if (userLogged) {
+    userLogged = JSON.parse(userLogged);
+    userInfo.style = "display: inline-block";
+    user.innerText = userLogged.email;
+  }
+
+  if (googleUserData) {
+    userInfo.style = "display: inline-block";
+    user.innerText = profile.getEmail();
+  }
 });
