90d540793d0b7d82b38667041271f6226ec22aab
freddie41 <fcl41@me.com>
2021-10-18 12:34:40 -0300

    Fix en funcion para mostrar forms de tipo de pago

--- js/cart.js
+++ js/cart.js
@@ -250,8 +250,8 @@ function showProductsCount(array) {
 function showPaymentForms() {
 
   let radios = document.getElementsByName("paymentType");
-  let cardPaymentForm = document.getElementById("bankPaymentForm");
-  let bankPaymentForm = document.getElementById("cardPaymentForm");
+  let cardPaymentForm = document.getElementById("cardPaymentForm");
+  let bankPaymentForm = document.getElementById("bankPaymentForm");
 
   cardPaymentForm.style.display = 'none';
   bankPaymentForm.style.display = 'none';
@@ -260,14 +260,15 @@ function showPaymentForms() {
     radios[i].onclick = function () {
 
       let val = this.value;
+      console.log(val)
 
       if (val == 'cardsradio') {
-        cardPaymentForm.style.display = 'none';
-        bankPaymentForm.style.display = 'block';
-      }
-      if (val == 'banksradio') {
         cardPaymentForm.style.display = 'block';
         bankPaymentForm.style.display = 'none';
+      }
+      if (val == 'banksradio') {
+        cardPaymentForm.style.display = 'none';
+        bankPaymentForm.style.display = 'block';
       }
     }
   }
