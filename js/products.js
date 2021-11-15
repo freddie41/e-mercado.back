b9756cbcc3243ee1ea2b942068d207f9ad6e3279
freddie41 <fcl41@me.com>
2021-11-14 22:07:08 -0300

    Se agrega funcion de agregar articulos al carrito

--- js/products.js
+++ js/products.js
@@ -3,6 +3,15 @@ const ORDER_ASC_BY_COST = "Menor precio";
 const ORDER_DESC_BY_COST = "Mayor precio";
 const ORDER_BY_PROD_RELEVANCE = "Relevancia";
 
+//Configuración de botones de swal2 para mostrar btns de BS.
+var swalBSCancelAcceptButtons = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-info m-3',
+        cancelButton: 'btn btn-secondary m-3'
+    },
+    buttonsStyling: false
+});
+
 // Variables de control de ingreso para el filtro de min y max $$$
 var minCost = undefined;
 var maxCost = undefined;
@@ -15,6 +24,9 @@ var foundProducts = undefined;
 
 // Lista para llenar con los productos y propiedades
 var currentProductsArray = [];
+
+//Lista para guardar instancias de articulos agregados al carrito.
+var newCartArtsArray = [];
 
 // Función para ordenar los productos en base a los criterios:
 // * Ascendente y descendente en base al precio.
@@ -68,10 +80,11 @@ function sortProducts(criteria, array) {
 
 // Función para mostrar info de producto al cliquear en una de las opciones de la lista
 function setProductInfo (id) {
-    localStorage.setItem("productID", JSON.stringify({productID: id}));
+    localStorage.setItem("productID", JSON.stringify({
+        productID: id,
+    }));
     window.location = "product-info.html";
 }
-
 
 // Muestro todos los productos de la lista en formato grid con su imagen, nombre, descripcion y precio
 function showProductsGrid() {
@@ -92,16 +105,21 @@ function showProductsGrid() {
 
                 htmlContentToAppend += `
                 <div class="col-12 col-md-6 col-lg-4">
-                    <a href="#" class="card mb-3 custom-card" onclick="setProductInfo('${product.id}');">
-                        <img class="bd-placeholder-img card-img-top" src="${product.imgSrc}">
-                        <h4 class="m-3">${product.name}</h4>
+                    <div class="card mb-3 custom-card">
+                        <div onclick="setProductInfo('${product.id}');" style="cursor: pointer;"> 
+                            <img class="bd-placeholder-img card-img-top" src="${product.imgSrc}">
+                            <h4 class="m-3">${product.name}</h4>
+                        </div>
                         <div class="card-body pt-0">
                             <p class="card-text">${product.description}</p>
-                            <div class="row justify-content-end">
-                                <div class="col-12 text-info text-right">${product.currency + " " + product.cost}</div>
+                            <div class="row align-items-center">
+                                <div class="col-7 text-left">
+                                    <button type="button" class="btn btn-info btn-sm" onclick="addProdtoCart(${product.id},'${product.name}', '${product.cost}', '${product.currency}', '${product.imgSrc}')">Agregar al carrito</button>
+                                </div>
+                                <div class="col-5 text-info text-right">${product.currency + " " + product.cost}</div>
                             </div>
                         </div>
-                    </a>
+                    </div>
                 </div>
                 `
             }
@@ -129,28 +147,70 @@ function showProductsList() {
                 product.description.toLowerCase().indexOf(foundProducts) != -1) {
 
                 htmlContentToAppend += `
-                <a href="#" class="list-group-item list-group-item-action" onclick="setProductInfo('${product.id}');">
+                <div class="list-group-item list-group-item-action">
                     <div class="row">
-                        <div class="col-3">
+                        <div class="col-3 onclick="setProductInfo('${product.id}');" style="cursor: pointer;">
                             <img src="${product.imgSrc}" alt="${product.description}" class="img-thumbnail">
                         </div>
                         <div class="col">
-                            <div class="d-flex w-100 justify-content-between">
-                                <div class="mb-1">
-                                    <h4 class="mb-1">${product.name}</h4>
-                                    <p>${product.description}</p>
+                            <div class="row d-flex">
+                                <div class="col-9" onclick="setProductInfo('${product.id}');" style="cursor: pointer;">
+                                    <h4 class="mb-2">${product.name}</h4>
+                                    <div>${product.description}</div>
                                 </div>
-                                <small class="text-info">${product.currency + " " + product.cost}</small>
+                                <div class="col-3">
+                                    <p class="text-info text-right my-1">${product.currency + " " + product.cost}</p>
+                                </div>
+                            </div>
+                            <div class="row d-flex">
+                                <div class="col text-right mt-2">
+                                    <button type="button" class="btn btn-info btn-sm" onclick="addProdtoCart(${product.id},'${product.name}', '${product.cost}', '${product.currency}', '${product.imgSrc}')">Agregar al carrito</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </a>
+                </div>
                 `
             }
         }
-
         document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
     }
+}
+
+//Funcion para agregar producto al carrito.
+function addProdtoCart(id, name, cost, currency, image) {
+
+    //Agrego instancia de articulo al objeto guardado.
+    newCartArtsArray.push(
+        {
+            id: parseInt(id),
+            name: name,
+            count: 1,
+            unitCost: parseInt(cost),
+            currency: currency,
+            src: image
+        }
+    );
+    //Guardo los cambios en el nuevo objeto en local.
+    localStorage.setItem('newCartArts', (JSON.stringify(
+        {
+            articles: newCartArtsArray
+        }
+    )));
+    //Alerta de confirmación.
+    swalBSCancelAcceptButtons.fire({
+        title: '¡Éxito!',
+        text: "Producto agregado al carrito.",
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonText: 'Ver carrito',
+        cancelButtonText: 'Continuar comprando',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location = "cart.html";
+        }
+    });
 }
 
 // Función para mostrar los productos ordenados por el criterio elegido
@@ -208,7 +268,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
         showProductsGrid();
         showProductsList();
-
     });
 
     // Importación de btns de tipo de orden para invocar a la función
